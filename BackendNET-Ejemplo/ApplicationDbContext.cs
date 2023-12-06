f03326fd85c6150480ee7c18c66494fe5ab7aaf7
AlbertJQM <aj.qmujica@studentambassadors.com>
2023-12-06 01:07:02 -0400

    Generación del CursoController, Curso y ApplicationDbContext, adherido una migracion.

---
+++ BackendNET-Ejemplo/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BackendNET_Ejemplo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendNET_Ejemplo
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Curso> Curso { get; set; }
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) { }
+    }
+}
--- BackendNET-Ejemplo/Program.cs
+++ BackendNET-Ejemplo/Program.cs
@@ -1,11 +1,18 @@
+using BackendNET_Ejemplo;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+string mySqlConnection = "Server=localhost;Database=DBEjemplo;Uid=root;Pwd=muyiga128";
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
