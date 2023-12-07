b48ee8a506f9803c2d6d91ec07f47122ca58253d
AlbertJQM <aj.qmujica@studentambassadors.com>
2023-12-07 00:34:52 -0400

    Endpoints definidos para GET, POST, PUT y DELETE.

--- BackendNET-Ejemplo/Controllers/CursoController.cs
+++ BackendNET-Ejemplo/Controllers/CursoController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BackendNET_Ejemplo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +10,100 @@ namespace BackendNET_Ejemplo.Controllers
     [ApiController]
     public class CursoController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+        public CursoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         // GET: api/<CursoController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IActionResult> Get()
         {
-            return new string[] { "Azure", "Blazor" };
+            try
+            {
+                var listCursos = await _context.Curso.ToListAsync();
+                return Ok(listCursos);
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
         }
 
         // GET api/<CursoController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> Get(int id)
         {
-            return "value";
+            try
+            {
+                var curso = await _context.Curso.FindAsync(id);
+                if (curso == null)
+                {
+                    return NotFound();
+                }
+                return Ok(curso);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // POST api/<CursoController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] Curso curso)
         {
+            try
+            {
+                _context.Curso.Add(curso);
+                await _context.SaveChangesAsync();
+                return Ok(curso);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // PUT api/<CursoController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(int id, [FromBody] Curso curso)
         {
+            try
+            {
+                if(id != curso.Id)
+                {
+                    return BadRequest();
+                }
+                _context.Curso.Update(curso);
+                await _context.SaveChangesAsync();
+                return Ok(new { message = "Curso actualizado con éxito." });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // DELETE api/<CursoController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
+            try
+            {
+                var curso = await _context.Curso.FindAsync(id);
+                if(curso == null)
+                {
+                    return BadRequest();
+                }
+                _context.Curso.Remove(curso);
+                await _context.SaveChangesAsync();
+                return Ok(new { message = "Curso eliminado con éxito." });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
--- BackendNET-Ejemplo/Program.cs
+++ BackendNET-Ejemplo/Program.cs
@@ -13,6 +13,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+});
 
 var app = builder.Build();
 
@@ -22,6 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
